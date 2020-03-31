66572cbbb13b092a073117c0bc314b9c8713a119
Nancy Campos <nancycampos@Nancys-MBP.fios-router.home>
2020-03-30 22:16:25 -0400

    stuff

--- DVProject2-master/starter/static/js/appBasedOnhw.js
+++ DVProject2-master/starter/static/js/appBasedOnhw.js
@@ -41,18 +41,42 @@ d3.csv("../static/js/Top3Corals.csv").then(function(finalcorals) {
   Plotly.newPlot("topcorals", data);
 });
 //observations over time trace 3-5
-d3.csv("../static/js/gorgoniancoral.csv").then(function(finalcorals2) {
+d3.csv("../static/js/coralagg.csv").then(function(finalcorals2) {
   console.log(finalcorals2)
+  function sum(input){
+             
+    if (toString.call(input) !== "[object Array]")
+       return false;
+         
+               var total =  0;
+               for(var i=0;i<input.length;i++)
+                 {                  
+                   if(isNaN(input[i])){
+                   continue;
+                    }
+                     total += Number(input[i]);
+                  }
+                return total;
+               }
   let year = [];
   finalcorals2.forEach(row=>{
     year.push(row.year);
   });
+  let cfreq = [];
+  finalcorals2.forEach(row=>{
+    cfreq.push(row.ccount)
+  })
+  let coralname = [];
+  finalcorals2.forEach(row=>{
+    coralname.push(row.commonname);
+  });
   var trace3 = {
-    x: year,
-    type: 'histograph',
+    x: coralname,
+    y: cfreq,
+    mode: 'markers',
   };
-  var data = [trace3];
-  Plotly.newPlot("gorgonianYear", data);
+  var data2 = [trace3];
+  Plotly.newPlot("gorgonianYear", data2);
 });
 
 //coral depths trace 4
